bdfad5cde5a3647556059c8aed89707293a74815
Shakhzodbek Yuldoshev <shakhzodbek.yuldoshev@gmail.com>
2019-12-01 08:54:22 +0500

    Updates

--- precache-manifest.0f13883e19d3c765f717d208c18fc381.js
+++ precache-manifest.0f13883e19d3c765f717d208c18fc381.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0b7040191bcf80068a14371b57f4e8c4",
+    "revision": "e0f292d93a02cae421b2dd0fc326672e",
     "url": "/react_deployment_demo/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react_deployment_demo/static/css/2.17e5ed98.chunk.css"
   },
   {
-    "revision": "e370e7096926e65d1a9b",
+    "revision": "f7a6628bfb539ea31e25",
     "url": "/react_deployment_demo/static/css/main.355a5bb3.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react_deployment_demo/static/js/2.45ad071a.chunk.js"
   },
   {
-    "revision": "e370e7096926e65d1a9b",
-    "url": "/react_deployment_demo/static/js/main.107534b7.chunk.js"
+    "revision": "f7a6628bfb539ea31e25",
+    "url": "/react_deployment_demo/static/js/main.da0def35.chunk.js"
   },
   {
     "revision": "b63b8bb0f0f52d8d51da",
